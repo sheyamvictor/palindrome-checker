27711476efc6ea7972c4169fb0f4532e45658e14
sheyamvictor <sheyamjoseph@gmail.com>
2024-06-25 17:22:49 +0530

    updated all files

--- script.js
+++ script.js
@@ -19,23 +19,32 @@ var cleanText = text.replace(/[^a-zA-Z0-9]/g ,"").toLowerCase();
 
 var isPlaindrome = cleanText===cleanText.split("").reverse("").join("");
 
-var message = isPlaindrome
-
-?`<span> yes.</span>It,s a plaindrome!`
-:`<span> no.</span>It,s not  plaindrome!`
+var message = isPlaindrome?`<span> yes.</span>It,s a plaindrome!`:`<span> no.</span>It,s not  plaindrome!`
 
 
 result.innerHTML=message;
 
 result.classList.remove("success","error");
 
-setTimeout(()=>{
+result.classList.add("success","error");
 
-    result.classList.add(isPlaindrome?"success":"error");
-
-
-  
-
-})
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
